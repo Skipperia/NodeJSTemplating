a0da1451466b2b4d4674f4b60c7cba1069763dc6
Alex Kipper <kippera27@gmail.com>
2023-09-07 20:52:55 +0300

    added some websocket action up in this bitch

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import { logger } from './utils/logger';
 import { apiRouter } from './routes';
 import { requestLogger, securiryMiddleWares } from './middleware';
+import expressWs from 'express-ws';
+import { WebSocket } from 'ws';
 
 
 
@@ -16,19 +18,48 @@ if (!secretKey) {
 }
 
 const app = express();
+const appWs = expressWs(app);
 app.use(express.json());
-
+const connectedWebsocketClients: WebSocket[] = [];
 
 //Add all routers
 app.use('/api', securiryMiddleWares.tokenAuth, securiryMiddleWares.sanitizeRequest, requestLogger, apiRouter);
 
+app.get('/about', (req: Request, res: Response) => {
+    logger.verbose("About route called");
+    res.json({
+        message: 'Welcome to the About page'
+    });
+});
+
 //Base of the app
 app.get('/', (req: Request, res: Response) => {
+    connectedWebsocketClients.forEach(item => item.send("called out base"))
     logger.verbose("Base route called");
     res.json({
         message: 'Welcome to the API'
     });
 });
+
+
+appWs.app.ws('/websocket', (ws: WebSocket, req) => {
+    connectedWebsocketClients.push(ws);
+    ws.on('open', () => {
+        ws.send("Connected to websocket!!!");
+    });
+    ws.on('message', (message: string) => {
+        ws.send("ayyy");
+    });
+
+    ws.on('close', () => {
+        const index = connectedWebsocketClients.indexOf(ws);
+        if (index !== -1) {
+            connectedWebsocketClients.splice(index, 1);
+        }
+    });
+});
+
+
 
 
 app.listen(5000, () => logger.info('Server started on port 5000'));
